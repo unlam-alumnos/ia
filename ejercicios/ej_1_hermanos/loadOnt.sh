e7680ab95b66b8c782f7483b2da59806ad0d7422
Cristian Miranda <crism60@gmail.com>
2018-08-29 15:38:22 -0300

    chore(structure): ejemplos & ejercicios

--- ejercicios/ej_1_hermanos/loadOnt.sh
+++ ejercicios/ej_1_hermanos/loadOnt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # keyspace is the graph name to load data
 
-keyspace='ej_1_parents'
+keyspace='ej_1_hermanos'
 
 graql console -f ./ontologies/base_ontology.gql -k $keyspace 
 graql console -f ./rules/base_rules.gql -k $keyspace
