3c55d8cf1ff0be3686b88938a40faa7708d84c1a
Cristian Miranda <crism60@gmail.com>
2018-08-30 11:37:16 -0300

    feat(ejercicio): Extra 2 - Ejemplo Red Semantica

---
+++ ejercicios/extras/ej_2_red_semantica/load.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# keyspace is the graph name to load data
+
+keyspace='ej_2_red_semantica'
+
+graql console -f ./ontologies/base_ontology.gql -k $keyspace 
+# graql console -f ./rules/base_rules.gql -k $keyspace
+graql console -f ./data/data.gql -k $keyspace
