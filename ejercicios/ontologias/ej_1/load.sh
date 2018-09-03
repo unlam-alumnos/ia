a9cfaef7ce0fd9650d004e0b01eca0accdad8a05
Cristian Miranda <crism60@gmail.com>
2018-09-03 17:46:21 -0300

    feat(ontologias): Ejercicio 1

--- ejercicios/ontologias/ej_1/load.sh
+++ ejercicios/ontologias/ej_1/load.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # keyspace is the graph name to load data
 
-keyspace='ej_3_principe'
+keyspace='ej_1_ontologias'
 
 graql console -f ./ontologies/base_ontology.gql -k $keyspace 
 graql console -f ./rules/base_rules.gql -k $keyspace
